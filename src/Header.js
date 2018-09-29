30bcf25b0c308b4020bb703f22d5f623fffce658
Ana Gheorghe <anagheorghe@DF-499109.local>
2018-09-29 17:10:08 -0400

    added header styles

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import logo from './images/logo.svg';
+import SimonHeader from './images/simon-header1.jpg';
 import './App.css';
 
 class Header extends Component {
   render() {
     return (
-      <header className="App-header">
-        <img alt="code-canada-logo" src={logo} />
+      <header className="App-header" style={{display: "flex", justifyContent: "space-between"}}>
+        <img alt="code-canada-logo" src={logo} style={{position: "relative", left: "50px", top: "-30px"}}/>
+        <div>
+          <img alt="dropdown-menu-instructor" src={SimonHeader} style={{position: "relative", right: "70px", top: "-22px"}}/>
+        </div>
       </header>
     );
   }
