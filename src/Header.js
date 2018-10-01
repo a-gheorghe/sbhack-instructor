790a481b341acb4d89b1b978affa71f816c94cc1
Simon Grossman <simongrossman@DF-notag03082018.local>
2018-10-01 10:37:06 -0400

    update

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './images/logo.svg';
-import SimonHeader from './images/simon-header1.jpg';
+import SimonHeader from './images/simon-header3x.jpg';
 import './App.css';
 
 class Header extends Component {
@@ -9,7 +9,7 @@ class Header extends Component {
       <header className="App-header" style={{display: "flex", justifyContent: "space-between"}}>
         <img alt="code-canada-logo" src={logo} style={{position: "relative", left: "50px", top: "-30px"}}/>
         <div>
-          <img alt="dropdown-menu-instructor" src={SimonHeader} style={{position: "relative", right: "70px", top: "-22px"}}/>
+          <img alt="dropdown-menu-instructor" src={SimonHeader} style={{position: "relative", right: "70px", top: "-10px", width:"110px", height: "24px"}}/>
         </div>
       </header>
     );
