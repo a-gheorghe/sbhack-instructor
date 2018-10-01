b1ac1015aa21a32d9e00374c4e4badd5accde3b7
Simon Grossman <simongrossman@DF-notag03082018.local>
2018-10-01 10:41:15 -0400

    remove redux

--- src/index.js
+++ src/index.js
@@ -2,14 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import reduxThunk from "redux-thunk";
-import reducers from "./reducers";
-import registerServiceWorker from './registerServiceWorker';
 
-import logger from 'redux-logger';
-const store = createStore(reducers, {}, applyMiddleware(reduxThunk, logger));
-
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById('root'));
--- src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-
-import answers from "./answers";
-
-export default combineReducers({
-  answers
-});
